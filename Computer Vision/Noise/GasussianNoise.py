96717781e3919bcec2689775d9e2d7abbfd6be03
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2023-08-23 08:25:46 +0900

    Update GasussianNoise.py

--- Computer Vision/Noise/GasussianNoise.py
+++ Computer Vision/Noise/GasussianNoise.py
@@ -8,15 +8,27 @@ import numpy as np
 import cv2
 import random
 
-
 def GaussianNoise(img, mean, sigma, ratio):
+    """
+    This function introduces Gaussian noise to an image.
+    
+    Parameters:
+    - img: input image
+    - mean: mean value of the Gaussian noise
+    - sigma: standard deviation of the Gaussian noise
+    - ratio: ratio of pixels that will be affected by the noise
+    
+    Returns:
+    - noise_img: image with Gaussian noise added
+    """
+    
     noise_img = img.copy()
     pixels_total = img.shape[0] * img.shape[1]
     noise_num = int(np.floor(pixels_total * ratio))
-    rand_num = np.array(random.sample(range(0, pixels_total), noise_num))  # 产生不重复的随机数
+    rand_num = np.array(random.sample(range(0, pixels_total), noise_num))  # Generate non-repeating random numbers
     x = rand_num // img.shape[1]
     y = rand_num % img.shape[1]
-    # noise_img[x, y] = noise_img[x, y] + int(random.gauss(mean, sigma))
+    
     gauss_img = np.random.normal(mean, sigma, img.shape)
     noise_img[x, y] = (noise_img + gauss_img)[x, y]
     noise_img = np.uint8(np.clip(noise_img, 0, 255))
@@ -28,14 +40,4 @@ if __name__ == '__main__':
     color = False
     if color:
         img = cv2.imread('lenna.png')
-        img_gaussian = GaussianNoise(img, 1, 4, 0.8)
-        img_merge = np.hstack([img, img_gaussian])
-        cv2.imwrite('Gaussian Image_Color.png', img_merge)
-    else:
-        img = cv2.imread('lenna.png', 0)
-        img_gaussian = GaussianNoise(img, 1, 4, 0.01)
-        img_merge = np.hstack([img, img_gaussian])
-        cv2.imwrite('Gaussian Image_Gray.png', img_merge)
-    cv2.imshow('Left: Original Image, Right: Gaussian Image', img_merge)
-
-    cv2.waitKey(0)
+        img_g
