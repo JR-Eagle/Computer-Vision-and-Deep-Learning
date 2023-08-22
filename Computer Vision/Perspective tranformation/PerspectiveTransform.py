fc4e0097871ee66a5c28ad65285481fbc68bd1ff
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2023-08-23 08:33:30 +0900

    Update PerspectiveTransform.py

--- Computer Vision/Perspective tranformation/PerspectiveTransform.py
+++ Computer Vision/Perspective tranformation/PerspectiveTransform.py
@@ -1,14 +1,24 @@
 """
-@author:Rai
-透视变换
+@author: Rai
+Perspective Transformation
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
 def getPerspectiveMatrix(src, dst):
+    """
+    Function to compute the perspective transformation matrix.
+
+    Parameters:
+    - src: Source points
+    - dst: Destination points
+
+    Returns:
+    - warpMatrix: The perspective transformation matrix
+    """
+    
     assert src.shape[0] == dst.shape[0] and src.shape[0] >= 4
 
     n = src.shape[0]
@@ -27,28 +37,22 @@ def getPerspectiveMatrix(src, dst):
     A = np.mat(A)
     warpMatrix = A.I * B
 
-    warpMatrix = np.array(warpMatrix).T[0]  # (1,8) → (8)
+    warpMatrix = np.array(warpMatrix).T[0]  # Reshaping from (1,8) to (8)
     warpMatrix = np.insert(warpMatrix, warpMatrix.shape, values=1.0)
     warpMatrix = warpMatrix.reshape(3, 3)
 
     return warpMatrix
 
-
 if __name__ == '__main__':
     src = np.float32([[207, 151], [517, 285], [17, 601], [343, 731]])
-    # dst = np.float32([[17, 151], [354, 151], [17, 639], [354, 639]])
     dst = np.float32([[0, 0], [337, 0], [0, 488], [337, 488]])
 
     warpMatrix = getPerspectiveMatrix(src, dst)
-    print('Perspective Transform Matrix :\n', warpMatrix)
+    print('Perspective Transform Matrix:\n', warpMatrix)
 
     img = cv2.imread('PerspectiveTransform_src.jpg')
     h, w, _ = img.shape
     img_dst = cv2.warpPerspective(img, warpMatrix, (337, 488))
-    cv2.imshow('Perspective Transform image', img_dst)
-    cv2.imwrite('PerspectiveTransofrm_dst.png', img_dst)
+    cv2.imshow('Perspective Transformed Image', img_dst)
+    cv2.imwrite('PerspectiveTransform_dst.png', img_dst)
     cv2.waitKey(0)
-
-    # img = plt.imread('PerspectiveTransform_src.jpg')
-    # plt.imshow(img)
-    # plt.show()
