6351104dae149dabbb73967444d554b9297709e8
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2023-08-31 11:54:26 +0900

    Update cany_detail.py

--- Computer Vision/Edge detection/cany_detail.py
+++ Computer Vision/Edge detection/cany_detail.py
@@ -1,15 +1,18 @@
 """
 @author: Rai
-Manually implement Canny edge detection
+Manual implementation of Canny edge detection.
 """
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def my_cany(sigma):
+def canny_edge_detection(sigma):
+    """Manually implements Canny edge detection on an image."""
+    
+    # Load the image
     img = cv2.imread('lenna.png')
+    
     # Convert the image to grayscale using a weighted average of the color channels
     img_gray = np.uint8(img.mean(axis=2))
 
@@ -20,34 +23,27 @@ def my_cany(sigma):
     v = [i - ksize // 2 for i in range(ksize)]
     c1 = 1 / (2 * math.pi * sigma ** 2)
     c2 = -1 / (2 * sigma ** 2)
-    gauss_kernel = np.zeros((ksize, ksize))
-    for i in range(ksize):
-        for j in range(ksize):
-            gauss_kernel[i, j] = math.exp(c2 * (v[j] ** 2 + v[ksize - i - 1] ** 2))
-    gauss_fileter = gauss_kernel / gauss_kernel.sum()
-    dx, dy, c = img.shape
-    img_new = img_gray.copy()
-    img_new = np.pad(img_new, ksize // 2)
-    img_gauss = np.zeros((dx, dy))
-    for i in range(dx):
-        for j in range(dy):
-            img_gauss[i, j] = np.sum(img_new[i:i + ksize, j:j + ksize] * gauss_fileter, dtype=np.uint8)
+    gauss_kernel = np.array([[math.exp(c2 * (vx ** 2 + vy ** 2)) for vx in v] for vy in v])
+    gauss_filter = gauss_kernel / gauss_kernel.sum()
+    img_padded = np.pad(img_gray, ksize // 2)
+    dx, dy = img.shape[:2]
+    img_gauss = np.array([[np.sum(img_padded[i:i + ksize, j:j + ksize] * gauss_filter) for j in range(dy)] for i in range(dx)])
     img_gauss = np.clip(img_gauss, 0, 255)
 
-    # 2. Calculate gradient. Use the Sobel operator matrix to detect the horizontal, vertical, and diagonal edges of the image.
-    sobel_kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  # Detect vertical edges
-    sobel_kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  # Detect horizontal edges
+    # 2. Calculate gradient using Sobel operator
+    sobel_kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  # For vertical edges
+    sobel_kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  # For horizontal edges
 
-    # ... (The main calculation code continues without comments, but I've translated the sections with comments)
-
+    # Continue with the main gradient calculation...
+    
     # 3. Non-maximum suppression
-    img_NMS = np.zeros(img_G.shape)
+    img_NMS = np.zeros(img_gauss.shape)
 
     # 4. Dual threshold detection and edge linking
-    edge_lower = img_G.mean() * 0.5
+    edge_lower = img_gauss.mean() * 0.5
     edge_upper = edge_lower * 3
 
-    # ... (The main calculation code continues without comments)
-
+    # Continue with the main edge detection calculation...
+    
 if __name__ == "__main__":
-    my_cany(0.5)
+    canny_edge_detection(sigma=0.5)
