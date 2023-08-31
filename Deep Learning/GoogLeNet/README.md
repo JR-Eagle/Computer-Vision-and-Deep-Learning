0c60b6b8cae260b53d6d2b62c38bbf4bb5cc5ff6
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2023-08-31 11:32:04 +0900

    Add files via upload

---
+++ Deep Learning/GoogLeNet/README.md
@@ -0,0 +1,11 @@
+## Task
+* GoogLeNet network training and inference
+
+  
+
+## 结果
+
+
+
+* 正确率：86.89%
+![精度曲线](./assets/Accuracy Curve.png)
---
+++ Deep Learning/GoogLeNet/predict.py
@@ -0,0 +1,47 @@
+"""
+@author: Rai
+Inference
+"""
+
+from model import GoogLeNet
+from torchvision.transforms import transforms
+import torch
+from PIL import Image
+
+
+def main():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    transform = transforms.Compose([
+         transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+         ])
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = GoogLeNet(num_classes=10)
+
+    net.load_state_dict(torch.load('./GoogLeNet.pth', map_location=device))
+    net.to(device)
+
+    img_path = '../data/car.jpg'
+    image = Image.open(img_path)
+    image = image.convert(mode='RGB')
+    image = transform(image)  # [ch, h, w]
+    image = torch.unsqueeze(image, 0)  # [ch, h, w] -> [1, ch, h, w]
+
+    # prediction
+    net.eval()
+    with torch.no_grad():
+        image = image.to(device)
+        output = net(image)
+        print(output)
+        pred = torch.max(output, dim=1)[1].item()
+    print('predict result:', classes[pred])
+
+
+if __name__ == '__main__':
+    main()
