6432ac67ba6c95e6bc392ac012858eb42ad37192
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2023-09-10 09:38:29 +0900

    Update GasussianNoise.py

--- Computer Vision/Noise/GasussianNoise.py
+++ Computer Vision/Noise/GasussianNoise.py
@@ -8,44 +8,51 @@ import numpy as np
 import cv2
 import random
 
-def GaussianNoise(img, mean, sigma, ratio):
+def add_gaussian_noise(image, mean, sigma, percentage):
     """
-    This function introduces Gaussian noise to an image.
+    Introduces Gaussian noise to an image.
     
     Parameters:
-    - img: input image
-    - mean: mean value of the Gaussian noise
-    - sigma: standard deviation of the Gaussian noise
-    - ratio: ratio of pixels that will be affected by the noise
+    - image: Input image.
+    - mean: Mean value of the Gaussian noise.
+    - sigma: Standard deviation of the Gaussian noise.
+    - percentage: Ratio of pixels that will be affected by the noise.
     
     Returns:
-    - noise_img: image with Gaussian noise added
+    - noised_image: Image with Gaussian noise added.
     """
     
-    noise_img = img.copy()
-    pixels_total = img.shape[0] * img.shape[1]
-    noise_num = int(np.floor(pixels_total * ratio))
-    rand_num = np.array(random.sample(range(0, pixels_total), noise_num))  # Generate non-repeating random numbers
-    x = rand_num // img.shape[1]
-    y = rand_num % img.shape[1]
+    noised_image = image.copy()
     
-    gauss_img = np.random.normal(mean, sigma, img.shape)
-    noise_img[x, y] = (noise_img + gauss_img)[x, y]
-    noise_img = np.uint8(np.clip(noise_img, 0, 255))
+    # Calculate the number of pixels to be noised
+    total_pixels_to_noise = int(percentage * image.shape[0] * image.shape[1])
+    
+    for _ in range(total_pixels_to_noise):
+        # Randomly select a pixel
+        rand_x = random.randint(0, image.shape[0] - 1)
+        rand_y = random.randint(0, image.shape[1] - 1)
+        
+        # Add Gaussian noise to the selected pixel
+        noised_image[rand_x, rand_y] += random.gauss(mean, sigma)
+        
+        # Clip the pixel value to be between 0 and 255
+        noised_image[rand_x, rand_y] = np.clip(noised_image[rand_x, rand_y], 0, 255)
 
-    return noise_img
-
+    return noised_image
 
 if __name__ == '__main__':
     color = False
     if color:
         img = cv2.imread('lenna.png')
-        img_gaussian = GaussianNoise(img, 1, 4, 0.8)
-        img_merge = np.hstack([img, img_gaussian])
+        # Add Gaussian noise to the color image
+        noised_image = add_gaussian_noise(img, 2, 4, 0.8)
+        img_merge = np.hstack([img, noised_image])
         cv2.imwrite('Gaussian Image_Color.png', img_merge)
     else:
         img = cv2.imread('lenna.png', 0)
-        img_gaussian = GaussianNoise(img, 1, 4, 0.01)
-        img_merge = np.hstack([img, img_gaussian])
+        # Add Gaussian noise to the grayscale image
+        noised_image = add_gaussian_noise(img, 2, 4, 0.8)
+        img_merge = np.hstack([img, noised_image])
         cv2.imwrite('Gaussian Image_Gray.png', img_merge)
     cv2.imshow('Left: Original Image, Right: Gaussian Image', img_merge)
+    cv2.waitKey(0)
